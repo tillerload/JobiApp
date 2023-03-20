145dd42517be7bc7fa737fb295780fbd248cfa45
tillerload <tyler.george.work@gmail.com>
2023-03-20 16:42:36 -0230

    actually installed tailwind this time lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <h1 className='uppercase underline'>wow!</h1>
     </div>
   )
 }
