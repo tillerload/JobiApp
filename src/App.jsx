925e3d80a97048cdcb99adacdff0042b5f2ab2a5
tillerload <tyler.george.work@gmail.com>
2023-03-20 19:27:35 -0230

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
 
+import { Link, Routes, Route } from 'react-router-dom'
+import ErrorPage from './routes/ErrorPage'
+
 function App() {
 
   return (
     <div className="App">
-      <h1 className='uppercase underline'>wow!</h1>
+      <nav>
+        <h1 className='uppercase underline'>wow!</h1>
+        <ul>
+          <li>
+            <Link to='/ErrorPage'>Dont Click this</Link>
+          </li>
+        </ul>
+      </nav>
+
+
+      <Routes>
+        <Route path='/ErrorPage' element={<ErrorPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return(
+    <div>
+      <h1>WOW WOW ERROR OUCHIE!</h1>
+    </div>
+  )
+}
+
+export default ErrorPage;
