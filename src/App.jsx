fdc7459e8d8cd48f79c53294e932a614b231528f
tillerload <tyler.george.work@gmail.com>
2023-03-21 15:47:31 -0230

    started html on HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,14 @@ import './App.css'
 
 import { Link, Routes, Route } from 'react-router-dom'
 import ErrorPage from './comps/ErrorPage'
+import HomePage from './comps/HomePage'
 
 function App() {
 
   return (
     <div className="App">
-      <nav>
-        <h1 className='uppercase underline'>wow!</h1>
-        <ul>
-          <li>
-            <Link to='/ErrorPage'>Dont Click this</Link>
-          </li>
-        </ul>
-      </nav>
-
-
+      <HomePage />
+      
       <Routes>
         <Route path='/ErrorPage' element={<ErrorPage />} />
       </Routes>
