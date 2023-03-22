d0744aa8374ddd024a0b39e5fa4796c2e2771594
tillerload <tyler.george.work@gmail.com>
2023-03-21 23:16:17 -0230

    finished html for index page and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import { Link, Routes, Route } from 'react-router-dom'
 import ErrorPage from './comps/ErrorPage'
 import HomePage from './comps/HomePage'
+import Footer from './comps/Footer'
 
 function App() {
 
   return (
     <div className="App">
       <HomePage />
-      
+      <Footer />
+
       <Routes>
         <Route path='/ErrorPage' element={<ErrorPage />} />
       </Routes>
---
+++ src/comps/Footer.jsx
@@ -0,0 +1,75 @@
+const Footer = () => {
+  return(
+    <footer>
+      <div className="wrapper">
+        <div className="footerTop">
+          <div className="footerLeft">
+            <h2>Most complete job portal.</h2>
+            <p>Signup and start finding your job or talents.</p>
+          </div>
+          <div className="footerRight">
+            <a href="">Looking for a job?</a>
+            <button>Post a job</button>
+          </div>
+        </div>
+        <div className="footerMiddle">
+          <ul>
+            <li>
+              <img src="" alt="Jobi Logo" />
+            </li>
+            <li>
+              <h4>Products</h4>
+              <li>Take the tour</li>
+              <li>Live chat</li>
+              <li>Self service</li>
+              <li>Mobile</li>
+              <li>Collaboration</li>
+              <li>Reviews</li>
+            </li>
+            <li>
+              <h4>Links</h4>
+              <li>Pricing</li>
+              <li>About us</li>
+              <li>Careers</li>
+              <li>Blog</li>
+            </li>
+            <li>
+              <h4>Legal</h4>
+              <li>Terms of use</li>
+              <li>Terms & conditions</li>
+              <li>Privacy</li>
+              <li>Cookie policy</li>
+            </li>
+            <li>
+              <h4>Newsletter</h4>
+              <p>Join & get important new regularly</p>
+              <form action="">
+                <input type="text" placeholder="Enter your email" />
+                <button>Send</button>
+              </form>
+              <p>We only send interesting and relevant emails</p>
+            </li>
+          </ul>
+        </div>
+        <div className="footerBottom">
+          <div className="footerBottomLeft">
+            <p>Privacy & Terms.</p>
+            <p>Contact Us</p>
+          </div>
+          <div className="footerBottomMiddle">
+            <p>Created with love by Tyler George 2023</p>
+          </div>
+          <div className="footerBottomRight">
+            <ul>
+              <li>F</li>
+              <li>I</li>
+              <li>P</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
