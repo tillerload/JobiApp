9244a2485401a6f0d562cbc36908fe0ef2d98f4e
tillerload <tyler.george.work@gmail.com>
2023-03-25 19:34:17 -0230

    installed firebase and linked the database

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBHW_3tk9uE4q1Q6TD9iUKNqZTtOgBTIS4",
+
+  authDomain: "jobiapp-281ce.firebaseapp.com",
+
+  projectId: "jobiapp-281ce",
+
+  storageBucket: "jobiapp-281ce.appspot.com",
+
+  messagingSenderId: "481465063148",
+
+  appId: "1:481465063148:web:3c827a7ed78e3dcd1cf7c9"
+
+};
+
+
+// Initialize Firebase
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
