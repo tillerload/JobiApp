aff1216d7efc8dbbd691362b224078470bbcd511
tillerload <tyler.george.work@gmail.com>
2023-04-27 17:07:57 -0230

    added placeholder image and icons to homepage header

--- src/pages/homePage/HomePageHeader.jsx
+++ src/pages/homePage/HomePageHeader.jsx
@@ -5,19 +5,27 @@ const HomePageHeader = () => {
           <div className="wrapper">
             <div className="flex">
               <div className="indexHeaderLeft">
-                <h5>#1 Online Marketplace</h5>
+                <h5 className="">#1 Online Marketplace</h5>
                 <h1>Find the talents for any job.</h1>
                 <p>Unlock your potential with quiality job and earn from world leading brands & co.</p>
                 <a href="">Post a job</a>
-                <div className="trustedBrands">
+                <div className="trustedBrands flex">
                   <p>Trusted by:</p>
-                  <img src="" alt="payoneer's logo" />
-                  <img src="" alt="google's logo" />
-                  <img src="" alt="adobe's logo" />
+                  <div>
+                    <img src="assets\logoPayoneer.png" alt="payoneer's logo" />
+                  </div>
+                  <div>
+                    <img src="assets\logoGoogle.png" alt="google's logo" />
+                  </div>
+                  <div>
+                    <img src="assets\logoAdobe.png" alt="adobe's logo" /> 
+                  </div>
                 </div>
               </div>
               <div className="indexHeaderRight">
-                <img src="" alt="drawing of a lady of her laptop" />
+                <div>
+                  <img src="https://placehold.jp/836x570.png" alt="drawing of a lady of her laptop" />
+                </div>
               </div>
             </div>
           </div>
