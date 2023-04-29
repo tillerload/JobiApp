077cbc8b97142413a82ff8f780971fe4558ffded
tillerload <tyler.george.work@gmail.com>
2023-04-29 14:26:58 -0230

    started stlying explore marketplace section

--- src/pages/homePage/HomePageExplore.jsx
+++ src/pages/homePage/HomePageExplore.jsx
@@ -4,37 +4,49 @@ const HomePageExplore = () => {
       <section className="marketplaceSection">
           <div className="wrapper">
             <div className="topPartMarketplace flex justify-between">
-              <h3>Explore the marketplace.</h3>
+              <h3 className="text-darkGreen text-3xl">Explore the marketplace.</h3>
               <a href="">Explore all fields</a>
             </div>
-            <ul>
-              <li>
-                <img src="assets\iconDesign.png" alt="pencil icon" />
+            <ul className="flex my-12 justify-evenly">
+              <li className="flex flex-col my-12">
+                <div>
+                  <img src="assets\iconDesign.png" alt="pencil icon" />
+                </div>
                 <h5>UI/UX Design</h5>
                 <p>12k+ Jobs</p>
               </li>
               <li>
-                <img src="assets\iconTags.png" alt="bracket icon" />
+                <div>
+                  <img src="assets\iconTags.png" alt="bracket icon" />
+                </div>
                 <h5>Development</h5>
                 <p>8k+ Jobs</p>
               </li>
               <li>
-                <img src="assets\iconBriefcase.png" alt="briefcase icon" />
+                <div>
+                  <img src="assets\iconBriefcase.png" alt="briefcase icon" />
+                </div>
                 <h5>Marketing</h5>
                 <p>12k+ Jobs</p>
               </li>
               <li>
-                <img src="assets\iconPhone.png" alt="telephone icon" />
+                <div>
+                  <img src="assets\iconPhone.png" alt="telephone icon" />
+                </div>
                 <h5>Telemarketing</h5>
                 <p>12k+ Jobs</p>
               </li>
               <li>
-                <img src="assets\iconEditing.png" alt="editing icon" />
+                <div>
+                  <img src="assets\iconEditing.png" alt="editing icon" />
+                </div>
                 <h5>Editing</h5>
                 <p>12k+ Jobs</p>
               </li>
               <li>
-                <img src="assets\iconBank.png" alt="bank icon" />
+                <div>
+                  <img src="assets\iconBank.png" alt="bank icon" />
+                </div>
                 <h5>Accounting</h5>
                 <p>12k+ Jobs</p>
               </li>
