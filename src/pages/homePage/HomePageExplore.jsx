8e4c6a6c9774b1b1580338d9642f81aad29acd60
tillerload <tyler.george.work@gmail.com>
2023-04-29 16:13:47 -0230

    almost done stlying marketplace section just need to align icons and create hover effect

--- src/pages/homePage/HomePageExplore.jsx
+++ src/pages/homePage/HomePageExplore.jsx
@@ -5,50 +5,50 @@ const HomePageExplore = () => {
           <div className="wrapper">
             <div className="topPartMarketplace flex justify-between">
               <h3 className="text-darkGreen text-3xl">Explore the marketplace.</h3>
-              <a href="">Explore all fields</a>
+              <a href="" className="text-limeGreen border-b">Explore all fields</a>
             </div>
             <ul className="flex my-12 justify-evenly">
-              <li className="flex flex-col my-12">
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconDesign.png" alt="pencil icon" />
                 </div>
-                <h5>UI/UX Design</h5>
-                <p>12k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">UI/UX Design</h5>
+                <p className="text-sm text-greyGreen">12k+ Jobs</p>
               </li>
-              <li>
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconTags.png" alt="bracket icon" />
                 </div>
-                <h5>Development</h5>
-                <p>8k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">Development</h5>
+                <p className="text-sm text-greyGreen">8k+ Jobs</p>
               </li>
-              <li>
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconBriefcase.png" alt="briefcase icon" />
                 </div>
-                <h5>Marketing</h5>
-                <p>12k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">Marketing</h5>
+                <p className="text-sm text-greyGreen">12k+ Jobs</p>
               </li>
-              <li>
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconPhone.png" alt="telephone icon" />
                 </div>
-                <h5>Telemarketing</h5>
-                <p>12k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">Telemarketing</h5>
+                <p className="text-sm text-greyGreen">12k+ Jobs</p>
               </li>
-              <li>
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconEditing.png" alt="editing icon" />
                 </div>
-                <h5>Editing</h5>
-                <p>12k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">Editing</h5>
+                <p className="text-sm text-greyGreen">12k+ Jobs</p>
               </li>
-              <li>
+              <li className="marketBtn">
                 <div>
                   <img src="assets\iconBank.png" alt="bank icon" />
                 </div>
-                <h5>Accounting</h5>
-                <p>12k+ Jobs</p>
+                <h5 className="text-lg my-6 text-darkGreen">Accounting</h5>
+                <p className="text-sm text-greyGreen">12k+ Jobs</p>
               </li>
             </ul>
           </div>
