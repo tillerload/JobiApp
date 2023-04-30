5025512d921735651f2ea2bcbc482dfb95bf9ca6
tillerload <tyler.george.work@gmail.com>
2023-04-30 15:17:00 -0230

    styled list section on homepage it may also need to be turned into a tags oops

--- src/pages/homePage/HomePageList.jsx
+++ src/pages/homePage/HomePageList.jsx
@@ -1,53 +1,53 @@
 const HomePageList = () => {
   return(
     <div>
-      <section className="listSection">
+      <section className="listSection my-10">
           <div className="wrapper">
-            <ul>
+            <ul className="flex justify-evenly">
               <li>
-                <h4>Trending Skills</h4>
+                <h4 className="text-2xl font-bold text-limeGreen">Trending Skills</h4>
                 <ul>
-                  <li>Blockchain</li>
-                  <li>Node.js</li>
-                  <li>HR Consulting</li>
-                  <li>Vue.js</li>
-                  <li>Microsoft Power Bl</li>
-                  <li>React.js</li>
-                  <li>Videographers</li>
+                  <li className="my-2">Blockchain</li>
+                  <li className="my-2">Node.js</li>
+                  <li className="my-2">HR Consulting</li>
+                  <li className="my-2">Vue.js</li>
+                  <li className="my-2">Microsoft Power Bl</li>
+                  <li className="my-2">React.js</li>
+                  <li className="my-2">Videographers</li>
                 </ul>
               </li>
               <li>
-                <h4>Top Skills</h4>
+                <h4 className="text-2xl font-bold">Top Skills</h4>
                 <ul>
-                  <li>Full Consultation</li>
-                  <li>Code Review</li>
-                  <li>Staff Augmentation</li>
-                  <li>Support</li>
-                  <li>Video Editors</li>
-                  <li>Data Entry Specialists</li>
-                  <li>Data Analyst</li>
+                  <li className="my-2">Full Consultation</li>
+                  <li className="my-2">Code Review</li>
+                  <li className="my-2">Staff Augmentation</li>
+                  <li className="my-2">Support</li>
+                  <li className="my-2">Video Editors</li>
+                  <li className="my-2">Data Entry Specialists</li>
+                  <li className="my-2">Data Analyst</li>
                 </ul>
               </li>
               <li>
-                <h4>Top in USA</h4>
+                <h4 className="text-2xl font-bold">Top in USA</h4>
                 <ul>
-                  <li>Technical Support</li>
-                  <li>Accountants</li>
-                  <li>Web Designers in US</li>
-                  <li>Customer identity</li>
-                  <li>Data entry</li>
+                  <li className="my-2">Technical Support</li>
+                  <li className="my-2">Accountants</li>
+                  <li className="my-2">Web Designers in US</li>
+                  <li className="my-2">Customer identity</li>
+                  <li className="my-2">Data entry</li>
                 </ul>
               </li>
               <li>
-                <h4>Project Catalog</h4>
+                <h4 className="text-2xl font-bold">Project Catalog</h4>
                 <ul>
-                  <li>Microsites</li>
-                  <li>Marketing Automation</li>
-                  <li>SEO & SMM</li>
-                  <li>Lead Generation</li>
-                  <li>Article Writing Services</li>
-                  <li>SEO Services</li>
-                  <li>Translation Services</li>
+                  <li className="my-2">Microsites</li>
+                  <li className="my-2">Marketing Automation</li>
+                  <li className="my-2">SEO & SMM</li>
+                  <li className="my-2">Lead Generation</li>
+                  <li className="my-2">Article Writing Services</li>
+                  <li className="my-2">SEO Services</li>
+                  <li className="my-2">Translation Services</li>
                 </ul>
               </li>
             </ul>
